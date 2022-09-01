f7e110dba49e66a2a6dde42ab611a3fc558ebb71
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-09-01 11:59:38 -0400

    update

---
+++ Ejemplo1_003/src/Ciclos2.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+
+
+public class Ciclos2 {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        int opcion = 0;
+        int apruebo = 0;
+        int rechazo = 0;
+        int otro = 0;
+        
+        while(opcion != 4)
+        {
+            System.out.println("Indique su voto:");
+            System.out.println("1.- Apruebo");
+            System.out.println("2.- Rechazo");
+            System.out.println("3.- Nulo o Blanco");
+            System.out.println("4.- Cerra mesa");
+            System.out.print("Ingrese su opcion:");
+            opcion = leer.nextInt();
+
+            if(opcion < 1 || opcion > 4)
+                System.out.println("Opción no es válido");
+            else
+            {
+                if(opcion == 1)
+                    apruebo++;
+                else if(opcion == 2)
+                    rechazo++;
+                else if(opcion == 3)
+                    otro++;      
+            }
+        }
+    }    
+}
