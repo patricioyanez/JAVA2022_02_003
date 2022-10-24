1c36e15c3a044326a2d26508e80a6221f55ff4fb
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-10-24 13:02:01 -0300

    Experiencia 5

---
+++ Exp 5 y 6/InterfazGrafica_003D/src/Modelo/Persona.java
@@ -0,0 +1,64 @@
+
+package Modelo;
+
+public class Persona {
+    private String rut;
+    private String nombre;
+    private String direccion;
+    private String telefono;
+
+    public Persona() {
+        this.rut = "";
+        this.nombre = "";
+        this.direccion = "";
+        this.telefono = "";
+    }
+    public Persona(String rut, String nombre, String direccion, String telefono) {
+        this.rut = rut;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public void limpiar()
+    {
+        this.rut = "";
+        this.nombre = "";
+        this.direccion = "";
+        this.telefono = ""; 
+    }
+    
+    
+}
