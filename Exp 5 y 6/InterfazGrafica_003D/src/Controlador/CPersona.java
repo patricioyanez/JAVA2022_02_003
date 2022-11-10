15f554926ced720ac6b0aeac45b5e2c8ae566b0e
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-11-10 12:26:57 -0300

    update

--- Exp 5 y 6/InterfazGrafica_003D/src/Controlador/CPersona.java
+++ Exp 5 y 6/InterfazGrafica_003D/src/Controlador/CPersona.java
@@ -2,13 +2,34 @@ package Controlador;
 import Conexion.Conexion;
 import Modelo.Persona;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class CPersona {
     private Conexion conexion = new Conexion();
    
     public boolean agregar(Persona persona)
     {
-        Connection cnx = conexion.obtenerConexion();
-        String sql = "INSERT INTO PERSONA (RUT, NOMBRE, APELLIDO, DIRECCION) VALUE (?,?,?,?)";
+        try {
+            Connection cnx = conexion.obtenerConexion();
+            String sql = "INSERT INTO PERSONA (RUT, NOMBRE, APELLIDO, DIRECCION) VALUE (?,?,?,?)";
+            
+            PreparedStatement stmt = cnx.prepareStatement(sql);
+            stmt.setString(1, persona.getRut());
+            stmt.setString(2, persona.getNombre());
+            stmt.setString(3, persona.getApellido());
+            stmt.setString(4, persona.getDireccion());
+            
+            stmt.executeUpdate();
+            stmt.close();
+            cnx.close();
+            return true;
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(CPersona.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
     }
 }
