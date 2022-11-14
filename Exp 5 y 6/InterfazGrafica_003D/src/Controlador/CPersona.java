3acacfcc7941efb0f74ec3248fdf2233172877eb
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-11-14 12:41:16 -0300

    update

--- Exp 5 y 6/InterfazGrafica_003D/src/Controlador/CPersona.java
+++ Exp 5 y 6/InterfazGrafica_003D/src/Controlador/CPersona.java
@@ -3,6 +3,7 @@ import Conexion.Conexion;
 import Modelo.Persona;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -31,5 +32,33 @@ public class CPersona {
             Logger.getLogger(CPersona.class.getName()).log(Level.SEVERE, null, ex);
         }
         return false;
+    }
+    
+    public Persona buscarPorRut(String rut)
+    {
+        try {
+            Connection cnx = conexion.obtenerConexion();
+            String sql = "SELECT * FROM PERSONA WHERE RUT = ?";
+            PreparedStatement ps = cnx.prepareStatement(sql);
+            ps.setString(1, rut);
+            ResultSet rs = ps.executeQuery();
+            
+            if(rs.next())
+            {
+                Persona persona = new Persona();
+                persona.setRut(rs.getString("RUT"));
+                persona.setNombre(rs.getString("NOMBRE"));
+                persona.setApellido(rs.getString("APELLIDO"));
+                persona.setDireccion(rs.getString("DIRECCION"));
+                return persona;
+            }
+            else
+                return null;
+            
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(CPersona.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
     }
 }
